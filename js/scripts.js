c053b7f65c65a63d23b2cfda8d78397858037aa9
mich-man <surenkov1985@gmail.com>
2024-01-17 14:48:40 +0300

    верстка

--- js/scripts.js
+++ js/scripts.js
@@ -26,4 +26,56 @@ document.addEventListener("DOMContentLoaded", function () {
 		closeModal();
 		scrollToElement(elem);
 	});
+
+	$(document).on("click", ".dropdown__value", function () {
+		const dropdown = $(this).closest(".dropdown");
+
+		$(".dropdown").each((ind, drop) => {
+			console.log($(this).closest($(drop)))
+			if (!$(this).closest($(drop)).length) {
+				$(drop).removeClass("active")
+			}
+		})
+
+		$(dropdown).toggleClass("active");
+	});
+
+	$(document).on("click", ".dropdown__item", function () {
+		const dropdown = $(this).closest(".dropdown");
+		const value = $(this).find(".value").text();
+
+		$(dropdown).find(".dropdown__value>.value").text(value);
+		$(dropdown).removeClass("active");
+	});
+
+	$(document).on("click", function (e) {
+
+		if (!e.target.closest(".dropdown")) {
+			$(".dropdown").removeClass("active");
+		}
+	});
+
+	$(document).on("change", "input[name=discount]", function () {
+		const input = $(this).closest(".slide-field__input");
+
+		console.log($("input[name=discount]:checked").val())
+
+		if ($("input[name=discount]:checked").val()) {
+			$(input).addClass("active");
+		}
+	});
+
+
+
+	$(document).on("mousedown", ".range-field__line .before", function (e) {
+
+		// let mousePositionX = e.
+
+		const range = $(this).closest(".range-field");
+		const minVal = $(range).find("input[type=range]").attr("min");
+		const maxVal = $(range).find("input[type=range]").attr("max");
+		const step = $(range).find("input[type=range]").attr("step");
+
+		console.log(e.offsetX)
+	})
 });
